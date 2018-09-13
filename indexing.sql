e41728636723ca2650622e5751da5fd21c66e4ab
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-09-12 19:20:05 -1000

    Add USER indexed_cars_user

--- indexing.sql
+++ indexing.sql
@@ -1,5 +1,7 @@
 -- Create a new postgres user named indexed_cars_user
 
+CREATE USER indexed_cars_user;
+
 -- Create a new database named indexed_cars owned by indexed_cars_user
 
 -- Run the provided scripts/car_models.sql script on the indexed_cars database
