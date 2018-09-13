c9b6c5f40f1a55acf04fc2404bb65dd8a4e96ada
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-09-12 19:19:20 -1000

    Add .env and docker-compose.yml

---
+++ indexing.sql
@@ -0,0 +1,11 @@
+-- Create a new postgres user named indexed_cars_user
+
+-- Create a new database named indexed_cars owned by indexed_cars_user
+
+-- Run the provided scripts/car_models.sql script on the indexed_cars database
+
+-- Run the provided scripts/car_model_data.sql script on the indexed_cars database 10 times
+
+-- there should be 223380 rows in car_models
+
+
