b197e9e741f9c176a39d1ed8d211ee9b4aab03b4
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-09-12 19:20:42 -1000

    Created database indexed_cars

--- indexing.sql
+++ indexing.sql
@@ -4,7 +4,11 @@ CREATE USER indexed_cars_user;
 
 -- Create a new database named indexed_cars owned by indexed_cars_user
 
+CREATE DATABASE indexed_cars;
+
 -- Run the provided scripts/car_models.sql script on the indexed_cars database
+
+
 
 -- Run the provided scripts/car_model_data.sql script on the indexed_cars database 10 times
 
